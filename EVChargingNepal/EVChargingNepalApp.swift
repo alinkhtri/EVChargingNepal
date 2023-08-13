95217cb3c532583125e2bb05c4be1265a24994b2
Alin Khatri <alin.khatri@tekkon.com.np>
2023-08-13 12:36:39 +0545

    Initial Commit

---
+++ EVChargingNepal/EVChargingNepalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVChargingNepalApp.swift
+//  EVChargingNepal
+//
+//  Created by Alin Khatri on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct EVChargingNepalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
